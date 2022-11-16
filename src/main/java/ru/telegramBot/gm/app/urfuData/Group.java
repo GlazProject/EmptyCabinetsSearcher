c91d926d536f840e71c493755384f29d08c67111
Nikita Glazunov <2004glaz@mail.ru>
2022-11-16 09:54:58 +0500

    Group.java переведена на record

--- src/main/java/ru/telegramBot/gm/app/urfuData/Group.java
+++ src/main/java/ru/telegramBot/gm/app/urfuData/Group.java
@@ -1,17 +1,3 @@
 package ru.telegramBot.gm.app.urfuData;
 
-public class Group {
-    private final int institute_id;
-    private final int id;
-
-    public Group(int m_id, int m_institute_id){
-        id = m_id;
-        institute_id = m_institute_id;
-    }
-    public String getId(){
-        String stringId = Integer.toString(id);
-        return stringId;
-        //return id;
-    }
-    public int getInstitute_id() {return institute_id;}
-}
+public record Group(int id, int instituteId) {}
