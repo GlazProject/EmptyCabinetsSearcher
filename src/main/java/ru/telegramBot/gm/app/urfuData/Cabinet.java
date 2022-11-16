f157708114f750ffd8b9b3b72129e48c1ed7879f
Nikita Glazunov <2004glaz@mail.ru>
2022-11-16 09:37:50 +0500

    Update Cabinet.java

--- src/main/java/ru/telegramBot/gm/app/urfuData/Cabinet.java
+++ src/main/java/ru/telegramBot/gm/app/urfuData/Cabinet.java
@@ -1,17 +1,45 @@
 package ru.telegramBot.gm.app.urfuData;
 
+import java.util.Arrays;
+
 public class Cabinet {
-    public Cabinet(String number, int lesson){
-        this.number = number;
-        this.status[lesson] = 1;
+    public final String Address;
+    public final int Number;
+
+    private static final int LESSONS_COUNT = 10;
+    private final boolean[] isBusy = new boolean[LESSONS_COUNT];
+
+    public Cabinet(String address, int number, int... busyLessons){
+        Number = number;
+        Address = address;
+        Arrays.fill(isBusy, false);
+        for (int lesson: busyLessons) {
+            occupy(lesson);
+        }
     }
 
-    private String address;
-    private final String number;
-    private int[] status = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-    public String getNumber(){
-        return this.number;
+    public boolean isBusy(int lessonNumber){
+        if (lessonNumber > isBusy.length || lessonNumber <= 0)
+            throw new IndexOutOfBoundsException();
+        return isBusy[lessonNumber-1];
     }
-    public void print() {System.out.println("print");};
+
+    public void occupy(int lessonNumber){
+        setIsBusyStatus(true, lessonNumber);
+    }
+
+    public void release(int lessonNumber){
+        setIsBusyStatus(false, lessonNumber);
+    }
+
+    private void setIsBusyStatus(boolean value, int number){
+        if (number > isBusy.length || number <= 0)
+            throw new IndexOutOfBoundsException();
+        isBusy[number-1] = value;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s, cabinet №%s", Address, Number);
+    }
 }
